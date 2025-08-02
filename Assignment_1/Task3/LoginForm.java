4e726fff52dc2c075fb53f61a08dd2d9a60918e6
snehalbhosale7519 <snehalbhosale7519@gmail.com>
2025-08-02 14:23:34 +0530

    Add files via upload

---
+++ Assignment_1/Task3/LoginForm.java
@@ -0,0 +1,13 @@
+
+package loginform;
+
+public class LoginForm {
+
+    
+    public static void main(String[] args) {
+      
+        LoginPage login = new LoginPage();
+        login.setVisible(true);
+    }
+    
+}
